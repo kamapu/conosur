bee1618096076345530b7ddf10154883676241fd
m_alvarez <m_alvarez@192.168.2.111>
2017-06-20 09:58:46 +0200

    Bugs in 'load_info'
    Output as list, jumping those cases with error

--- R/load_info.R
+++ R/load_info.R
@@ -15,26 +15,36 @@ load_info <- function(x, entries, labels, progress=TRUE) {
 	Info <- list()
 	if(progress) pb <- tkProgressBar(min=0, max=nrow(x), width=300)
 	for(i in rownames(x)) {
-		if(progress) {
-			Sys.sleep(0.1)
-			setTkProgressBar(pb, which(rownames(x) == i),
-					title=paste("Completing entries:",
-							round(which(rownames(x) == i)/nrow(x)*100,),
-							"% done"))
-		}
-		Page <- htmlTreeParse(x[i,"Link"], useInternalNodes=TRUE,
-				encoding="UTF-8")
-		Table <- readHTMLTable(Page, header=FALSE, stringsAsFactors=FALSE)[[3]]
-		Table[!is.na(Table[,2]) & Table[,2] == "",2] <- NA
-        Table[!is.na(Table[,2]) & Table[,2] == "-",2] <- NA
-        for(j in 1:ncol(Table)) {
-            Table[,j] <- iconv(Table[,j], to="LATIN1")
-        }
-        Info[[i]] <- Table[match(entries, Table[,1]),2]
-		names(Info[[i]]) <- labels
-	}
+        tryCatch({if(progress) {
+                        Sys.sleep(0.1)
+                        setTkProgressBar(pb, which(rownames(x) == i),
+                                title=paste("Completing entries:",
+                                        round(which(rownames(x) == i)/
+                                                        nrow(x)*100,),
+                                        "% done"))
+                    }
+                    Page <- htmlTreeParse(x[i,"Link"], useInternalNodes=TRUE,
+                            encoding="UTF-8")
+                    Table <- readHTMLTable(Page, stringsAsFactors=FALSE)[[3]]
+                    Table[!is.na(Table[,2]) & Table[,2] == "",2] <- NA
+                    Table[!is.na(Table[,2]) & Table[,2] == "-",2] <- NA
+                    ## for(j in 1:ncol(Table)) {
+                    ##     Table[,j] <- iconv(Table[,j], to="LATIN1")
+                    ## }
+                    Info[[i]] <- Table[match(iconv(entries,
+                                            to="ASCII//TRANSLIT"),
+                                    iconv(Table[,1], to="ASCII//TRANSLIT")) ,2]
+                    names(Info[[i]]) <- labels
+                }, error=function(e) {
+                    cat("ERROR :", conditionMessage(e), "\n")
+                }
+        )
+    }
 	if(progress) close(pb)
-	Info <- do.call(rbind, Info)
-	for(i in colnames(Info)) x[,i] <- iconv(Info[,i], to="LATIN1")
-	return(x)
+    ## Info <- do.call(rbind, Info)
+    ## for(i in colnames(Info)) x[,i] <- iconv(Info[,i], to="LATIN1")
+    ## return(x)
+    return(Info)
 }
+
+
